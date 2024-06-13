509bab7d5852a8098b93f37d866fbbc1eccd1f67
OMKAR BORATE <107358702+ombora@users.noreply.github.com>
2024-06-13 10:04:12 +0530

    Update Third.js

--- Assignment No 2/Third.js
+++ Assignment No 2/Third.js
@@ -1,1 +1,29 @@
+function findTax(salary) {
+    let tax;
 
+    switch (true) {
+        case (salary > 0 && salary <= 500000):
+            tax = salary * 0;
+            break;
+        case (salary > 500000 && salary <= 1000000):
+            tax = salary * 0.10;
+            break;
+        case (salary > 1000000 && salary <= 1500000):
+            tax = salary * 0.20;
+            break;
+        case (salary > 1500000):
+            tax = salary * 0.30;
+            break;
+        default:
+            tax = 'Error: Invalid salary input';
+    }
+
+    return tax;
+}
+
+// Example usage:
+console.log(findTax(400000));   // Output: 0
+console.log(findTax(750000));   // Output: 75000
+console.log(findTax(1250000));  // Output: 250000
+console.log(findTax(2000000));  // Output: 600000   
+console.log(findTax(-1000)); //Invalid Input
