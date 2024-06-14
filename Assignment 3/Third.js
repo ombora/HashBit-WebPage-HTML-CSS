fea0501490f02ef5f9051e313e311bb9f372e8f9
OMKAR BORATE <107358702+ombora@users.noreply.github.com>
2024-06-14 12:40:49 +0530

    Update Third.js

--- Assignment 3/Third.js
+++ Assignment 3/Third.js
@@ -1,1 +1,13 @@
+let string = 'INDIA';
+
+// Convert the string into an array of characters
+let charArray = string.split('');
+
+// Use splice to replace the last character 'A' with 'ONESIA'
+charArray.splice(3, 4, 'O', 'N', 'E', 'S', 'I', 'A');
+
+// Join the array back into a string
+let newString = charArray.join('');
+
+console.log(newString); // Output: 'INDONESIA'
 
