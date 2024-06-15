9db6bfe27b2859b4f2882e92bd05bee817ecb4d1
OMKAR BORATE <107358702+ombora@users.noreply.github.com>
2024-06-15 09:05:33 +0530

    Create Sixth.js

---
+++ Assignment 3/Sixth.js
@@ -0,0 +1,7 @@
+// Input array
+let inputArr = [1, 2, 3, 9, 10, 7, 5, 4, 3];
+
+// Use filter to get numbers greater than 5
+let result = inputArr.filter(number => number > 5);
+
+console.log(result); // Output: [9, 10, 7]
